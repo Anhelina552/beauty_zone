2386ecc1b8b85e65b3d6f0232e49747a8d745059
Anhelina552 <ilavskaanhelina@gmail.com>
2025-05-31 00:55:44 +0300

    Add gunicorn to requirements

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -11,8 +11,11 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 import os
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-default')
+
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -27,7 +30,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # SECURITY WARNING: keep the secret key used in production secret!
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
@@ -86,6 +89,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'default': dj_database_url.config(conn_max_age=600)
+
     }
 }
 
