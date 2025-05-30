8bc88aad89d433b7da868cdc57ac852c6fdb5b59
Anhelina552 <ilavskaanhelina@gmail.com>
2025-05-31 01:08:45 +0300

    Додано хост2

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -32,7 +32,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['beauty-zone-5.onrender.com', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['beauty-zone-6.onrender.com', 'localhost', '127.0.0.1']
 
 
 
